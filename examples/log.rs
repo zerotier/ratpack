c25fe9f9352a1f1755d1ad95365440a440a44212
Erik Hollensbe <linux@hollensbe.org>
2022-01-21 01:22:30 -0800

    cleanup imports
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- examples/log.rs
+++ examples/log.rs
@@ -1,7 +1,7 @@
-use http::{Request, Response, StatusCode};
+use http::{Request, Response};
 use hyper::Body;
 use log::LevelFilter;
-use ratpack::{app::App, compose_handler, handler::Params, Error, HTTPResult, ServerError};
+use ratpack::{app::App, compose_handler, handler::Params, HTTPResult, ServerError};
 
 async fn log(req: Request<Body>, resp: Option<Response<Body>>, _params: Params) -> HTTPResult {
     log::trace!("New request: {}", req.uri().path());
